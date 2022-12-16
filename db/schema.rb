d83accc11d97f2fa1cc7fb55d425b8cd06520977
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-12-17 01:32:31 +0300

    created user resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_154802) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_221225) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_154802) do
     t.string "checkoutRequestID"
     t.string "merchantRequestID"
     t.string "mpesaReceiptNumber"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.integer "phone_number"
+    t.integer "age"
+    t.string "gender"
+    t.string "password_digest"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
