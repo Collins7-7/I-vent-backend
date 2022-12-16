c05220849db59ae36296ca551f4b8144efed90b2
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-12-16 23:23:52 +0300

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:show, :index, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  get "/users", to: "users#index"
 
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+
+  
 
 
   # get '/hello', to: 'application#hello_world'
